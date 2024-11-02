f8bea1a454f0375e49666284dbfa753b2fbc725a
Aizar Farhan <andiaizarfarhan@gmail.com>
2024-11-02 20:26:08 +0700

    update component and add picture

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+// import Me from '../'
 
 const Hero = () => {
   return (
@@ -22,19 +25,29 @@ const Hero = () => {
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex-col items-center justify-center">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100">
-                Final Year Student / QA Internship
-            </h2>
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <img src="./assets/me.jpg" alt="Aizar" className="rounded-full w-48 h-48 mb-4 border-2 border-purple object-cover" />
+          
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100">
+            Final Year Student / QA Internship
+          </h2>
 
-            <TextGenerateEffect
+          <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Web Developer & Quality Assurance"
-            />
+            words="Quality Assurance"
+          />
 
-            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-                Hi, I&apos;m Aizar, a Web Developer and Quality Assurance based in Jakarta
-            </p>
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Aizar, a Quality Assurance based in Jakarta
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
